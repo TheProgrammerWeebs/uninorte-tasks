ff6fe60688203cc17246ae352455d846abf9d902
Jose David Padilla Gamarra <djgamarra@uninorte.edu.co>
2018-05-10 12:03:37 -0500

    Iniciando trabajos con base de datos de tareas

--- app/src/main/java/team/uninortetasks/uninortetasks/SplashScreen.java
+++ app/src/main/java/team/uninortetasks/uninortetasks/SplashScreen.java
@@ -2,12 +2,6 @@ package team.uninortetasks.uninortetasks;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.Date;
 
 import io.realm.Realm;
 import team.uninortetasks.uninortetasks.Database.TasksDB;
@@ -24,15 +18,4 @@ public class SplashScreen extends AppCompatActivity {
         TasksDB.init();
     }
 
-    public void onClick(View view) {
-        Toast.makeText(this, "Entra onclick", Toast.LENGTH_LONG).show();
-        EditText id = findViewById(R.id.id);
-        EditText name = findViewById(R.id.name);
-        EditText extra = findViewById(R.id.extra);
-        TextView date = findViewById(R.id.textView);
-        System.out.println("add");
-        TasksDB.add(this, Integer.parseInt(id.getText().toString()), name.getText().toString(), new Date());
-        System.out.println("get");
-        date.setText(TasksDB.get(Integer.parseInt(id.getText().toString())).getName());
-    }
 }
