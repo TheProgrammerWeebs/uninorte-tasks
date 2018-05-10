2833af8ecbe78dc3c52f044ba52c911d04398171
Jose David Padilla Gamarra <djgamarra@uninorte.edu.co>
2018-05-10 12:28:28 -0500

    Documentando metodos de manejador de BD

--- app/src/main/java/team/uninortetasks/uninortetasks/Database/TasksDB.java
+++ app/src/main/java/team/uninortetasks/uninortetasks/Database/TasksDB.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.widget.Toast;
 
 import java.util.Date;
+import java.util.Random;
 
 import javax.annotation.Nullable;
 
@@ -15,13 +16,38 @@ public class TasksDB {
 
     private static RealmResults<Task> all;
 
+    /**
+     * Inicializa la base de datos de tareas y obtiene la lista de todas las tareas registradas.
+     */
     public static void init() {
         Realm realm = Realm.getDefaultInstance();
         all = realm.where(Task.class).findAll();
     }
 
-    public static void add(final Context context, int id, String name, Date limit) {
-        final Task task = new Task(id, name, Priority.high, State.pending, Type.goal, limit);
+    /**
+     * Genera un Id aleatorio no existente para la creación de nuevas tareas.
+     *
+     * @return ID generado.
+     */
+    private static int generateId() {
+        Realm realm = Realm.getDefaultInstance();
+        Random random = new Random();
+        int id;
+        do {
+            id = random.nextInt(9999) + 1;
+        } while (realm.where(Task.class).equalTo("id", id).count() == 0);
+        return id;
+    }
+
+    /**
+     * Agrega una nueva tarea a la base de datos.
+     *
+     * @param context Contexto del que se llama al método (Activity...).
+     * @param name    Nombre de la tarea.
+     * @param limit   Tiempo limite de finalización.
+     */
+    public static void add(final Context context, String name, Date limit) {
+        final Task task = new Task(TasksDB.generateId(), name, Priority.high, State.pending, Type.goal, limit);
         Realm realm = Realm.getDefaultInstance();
         realm.executeTransaction(new Realm.Transaction() {
             @Override
@@ -35,6 +61,12 @@ public class TasksDB {
         });
     }
 
+    /**
+     * Obtiene la tarea con el ID pasado como parámetro.
+     *
+     * @param id ID de la tarea.
+     * @return Tarea encontrada o null si el ID no existe.
+     */
     @Nullable
     public static Task get(int id) {
         Realm realm = Realm.getDefaultInstance();
@@ -45,10 +77,19 @@ public class TasksDB {
         return null;
     }
 
+    /**
+     * Obtiene todas las tareas registradas en la base de datos.
+     *
+     * @return Lista de tareas.
+     */
     public static RealmResults<Task> getAll() {
         return all;
     }
 
+    /**
+     * @param context
+     * @param id
+     */
     public static void remove(final Context context, final int id) {
         Realm realm = Realm.getDefaultInstance();
         realm.executeTransaction(new Realm.Transaction() {
@@ -63,6 +104,9 @@ public class TasksDB {
         });
     }
 
+    /**
+     * @param task
+     */
     public static void edit(final Task task) {
         Realm realm = Realm.getDefaultInstance();
         realm.executeTransaction(new Realm.Transaction() {
--- app/src/main/java/team/uninortetasks/uninortetasks/SplashScreen.java
+++ app/src/main/java/team/uninortetasks/uninortetasks/SplashScreen.java
@@ -12,6 +12,7 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
+        //Inicializa la conexion con la base de datos de Realm
         Realm.init(this);
 //        RealmConfiguration config = new RealmConfiguration.Builder().deleteRealmIfMigrationNeeded().build();
 //        Realm.setDefaultConfiguration(config);
