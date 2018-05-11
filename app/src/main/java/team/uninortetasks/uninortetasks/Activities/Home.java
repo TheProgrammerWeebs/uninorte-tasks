b507c902a3f979dac6fd856521068f9b33410111
Jose David Padilla Gamarra <djgamarra@uninorte.edu.co>
2018-05-11 18:47:01 -0500

    Iniciando trabajos con el header

--- app/src/main/java/team/uninortetasks/uninortetasks/Activities/Home.java
+++ app/src/main/java/team/uninortetasks/uninortetasks/Activities/Home.java
@@ -1,7 +1,12 @@
 package team.uninortetasks.uninortetasks.Activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.internal.NavigationMenuView;
+import android.support.design.widget.NavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 import team.uninortetasks.uninortetasks.R;
 
@@ -11,5 +16,10 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_screen);
+        NavigationView nav = findViewById(R.id.navigator);
+        View header = nav.getHeaderView(0);
+        ImageView headerImg = header.findViewById(R.id.headerimg);
+        Bundle b = getIntent().getExtras();
+        headerImg.setImageDrawable(getResources().getDrawable(b.getInt("header")));
     }
 }
