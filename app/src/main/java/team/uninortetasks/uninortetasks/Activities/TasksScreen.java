800ff7c7a63479bd7a79a9970243deb29cbb02e8
Jose David Padilla Gamarra <djgamarra@uninorte.edu.co>
2018-05-23 11:57:15 -0500

    Trabajando en add category

--- app/src/main/java/team/uninortetasks/uninortetasks/Activities/TasksScreen.java
+++ app/src/main/java/team/uninortetasks/uninortetasks/Activities/TasksScreen.java
@@ -56,20 +56,18 @@ public class TasksScreen extends AppCompatActivity implements AddCategory.OnFrag
     private void loadCategories() {
         SubMenu menu = nav.getMenu().findItem(R.id.categories).getSubMenu();
         menu.clear();
-        nav.getMenu().removeItem();
         menu.add(R.string.for_today).setIcon(R.drawable.ic_today).setOnMenuItemClickListener(item -> {
             getSupportActionBar().setTitle(R.string.for_today);
             getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.darkRed)));
-
             loadView(null);
             item.setChecked(true);
             root.closeDrawers();
             return true;
-        }).setChecked(true);
+        });
         for (Category cat : Category.getAll()) {
             menu.add(cat.getName()).setIcon(cat.getIcon()).setOnMenuItemClickListener(item -> {
-                getSupportActionBar().setTitle(cat.getName());
                 getSupportActionBar().setBackgroundDrawable(new ColorDrawable(cat.getColor()));
+                getSupportActionBar().setTitle(cat.getName());
 
                 loadView(cat);
                 item.setChecked(true);
@@ -77,18 +75,18 @@ public class TasksScreen extends AppCompatActivity implements AddCategory.OnFrag
                 return true;
             });
         }
-        nav.getMenu().add("Agregar categoría").setIcon(R.drawable.ic_add).setOnMenuItemClickListener(item -> {
+        nav.getMenu().findItem(R.id.add).setOnMenuItemClickListener(item -> {
             getSupportFragmentManager().beginTransaction().replace(R.id.tasksContent, new AddCategory()).commit();
             root.closeDrawers();
             return true;
         });
-        nav.getMenu().add("Editar categoría").setIcon(R.drawable.ic_edit).setOnMenuItemClickListener(item -> {
+        nav.getMenu().findItem(R.id.edit).setOnMenuItemClickListener(item -> {
 
 
             root.closeDrawers();
             return true;
         });
-        nav.getMenu().add("Eliminar categoría").setIcon(R.drawable.ic_delete).setOnMenuItemClickListener(item -> {
+        nav.getMenu().findItem(R.id.delete).setOnMenuItemClickListener(item -> {
 
 
             root.closeDrawers();
