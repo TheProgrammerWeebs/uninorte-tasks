b53b19f075fbe99f03f04a55c8f3002cb1103c86
dan3143 <spark31417@gmail.com>
2018-05-26 14:46:26 -0500

    Avances en TaskAdapter

--- app/src/main/java/team/uninortetasks/uninortetasks/Activities/DreamCalculatorScreen.java
+++ app/src/main/java/team/uninortetasks/uninortetasks/Activities/DreamCalculatorScreen.java
@@ -1,14 +1,12 @@
 package team.uninortetasks.uninortetasks.Activities;
 
-import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
-import java.lang.reflect.Array;
-
 import team.uninortetasks.uninortetasks.R;
+
 
 public class DreamCalculatorScreen extends AppCompatActivity {
 
@@ -18,14 +16,9 @@ public class DreamCalculatorScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.screen_dreamcalculator);
-
         cdSpinner = findViewById(R.id.type_of_dreamer_Spinner);
-
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.valores_array,android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.valores_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
-
         cdSpinner.setAdapter(adapter);
-
-
     }
-    }
+}
