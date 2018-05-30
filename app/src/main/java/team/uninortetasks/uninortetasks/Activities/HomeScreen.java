4312835f929f9703e5d716e44eb7b71abdf21dd6
Jose David Padilla Gamarra <djgamarra@uninorte.edu.co>
2018-05-30 16:00:46 -0500

    Homescreen finished

--- app/src/main/java/team/uninortetasks/uninortetasks/Activities/HomeScreen.java
+++ app/src/main/java/team/uninortetasks/uninortetasks/Activities/HomeScreen.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
+import team.uninortetasks.uninortetasks.Database.Task;
 import team.uninortetasks.uninortetasks.Fragments.DashboardFragment;
 import team.uninortetasks.uninortetasks.Fragments.ForTodayFragment;
 import team.uninortetasks.uninortetasks.Others.TabsAdapter;
@@ -22,6 +24,8 @@ public class HomeScreen extends AppCompatActivity {
         viewPager = findViewById(R.id.viewPager);
         tabLayout = findViewById(R.id.tabLayout);
         TabsAdapter adapter = new TabsAdapter(getSupportFragmentManager());
+
+        ((TextView) findViewById(R.id.nForToday)).setText("Tareas para hoy: " + Task.tasksForToday().size());
 
         adapter.addFragment(new ForTodayFragment(), getResources().getString(R.string.for_today));
         adapter.addFragment(new DashboardFragment(), getResources().getString(R.string.all));
